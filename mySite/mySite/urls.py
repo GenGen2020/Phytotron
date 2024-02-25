88b067fb98c38531e0c8113c25eba48d82e2c85a
Wen Li <19wl43@queensu.ca>
2024-02-24 21:00:34 -0500

    Framework

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('', views.index, name='index'),
+    path('reservation/', views.reservation, name='reservation'),
 ]
